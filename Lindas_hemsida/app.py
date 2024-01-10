ef18d3be4db49aed6ca05d4957293bc48b68952c
John-John Carlsson <carlsson.johnjohn@gmail.com>
2024-01-10 14:45:28 +0100

    Lindas hemsida

---
+++ Lindas_hemsida/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, upgrade
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/lindasdb'
+
+db.app = app
+db.init_app(app)
+migrate = Migrate(app,db)
+
+# Kör först 'flask db init'
+# Kör sedan 'flask db migrate -m "Initial migration.'
+# Kör sedan 'flask db upgrade'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/bocker')
+def bocker():
+    return render_template('bocker.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        upgrade()
+
+    app.run(debug=True)
